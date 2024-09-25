192e18888c3b50b83de95bbdc08faafddd9ff95a
jjdavenport <jordanjamesdavenport@gmail.com>
2024-09-25 14:46:48 +0100

    add clear function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,50 @@ import Form from "./components/form";
 import Footer from "./components/footer";
 import Results from "./components/results";
 import Empty from "./components/empty";
+import { useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [term, setTerm] = useState("");
+  const [rate, setRate] = useState("");
+  const [button, setButton] = useState(false);
+  const [error, setError] = useState(false);
+
+  const check = (input) => {
+    if (input === "") {
+      setError("blank");
+    } else {
+      setError("");
+    }
+  };
+
+  const clear = () => {
+    setAmount("");
+    setRate("");
+    setTerm("");
+    setButton("");
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    check();
+  };
+
   return (
     <>
-      <Form />
+      <Form
+        onClick={clear}
+        onSubmit={submit}
+        amount={amount}
+        setAmount={setAmount}
+        term={term}
+        setTerm={setTerm}
+        rate={rate}
+        setRate={setRate}
+        button={button}
+        setButton={setButton}
+        error={error}
+      />
       <Empty />
       <Footer />
     </>
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,47 +1,93 @@
 import icon from "./assets/icon-calculator.svg";
 
-const Form = () => {
-  const submit = (e) => {
-    e.preventDefault();
-  };
-
-  const clear = () => {
-    setInput("");
+const Form = ({
+  onClick,
+  onSubmit,
+  rate,
+  setRate,
+  term,
+  setTerm,
+  amount,
+  setAmount,
+  error,
+  button,
+  setButton,
+}) => {
+  const change = (value, e) => {
+    if (value === "amount") {
+      setAmount(e.target.value);
+    } else if (value === "term") {
+      setTerm(e.target.value);
+    } else if (value === "rate") {
+      setRate(e.target.value);
+    } else {
+      return;
+    }
   };
 
   return (
     <>
       <main>
         <h1>Mortgage Calculator</h1>
-        <button className="underline" onClick={clear}>
+        <button className="underline" onClick={onClick}>
           Clear All
         </button>
-        <form noValidate onSubmit={submit} className="flex flex-col gap-4">
+        <form noValidate onSubmit={onSubmit} className="flex flex-col gap-4">
           <label className="flex flex-col">
             Mortgage Amount
             <div>
               <span>£</span>
-              <input type="number" />
+              <input
+                onChange={(e) => change("amount", e)}
+                value={amount}
+                type="number"
+              />
             </div>
+            {error && <span>{error}</span>}
           </label>
           <label className="flex flex-col">
             Mortgage Term
             <div className="flex flex-row-reverse">
               <span>years</span>
-              <input type="number" />
+              <input
+                onChange={(e) => change("term", e)}
+                value={term}
+                type="number"
+              />
             </div>
+            {error && <span>{error}</span>}
           </label>
           <label className="flex flex-col">
             Interest Rate
             <div className="flex flex-row-reverse">
               <span>%</span>
-              <input type="number" />
+              <input
+                onChange={(e) => change("rate", e)}
+                value={rate}
+                type="number"
+              />
             </div>
+            {error && <span>{error}</span>}
           </label>
           <label className="flex flex-col gap-2">
             Mortgage Type
-            <button>Repayment</button>
-            <button>Interest Only</button>
+            <button type="button" onClick={() => setButton("repayment")}>
+              Repayment
+              <div
+                className={`flex h-5 w-5 cursor-pointer items-center justify-center rounded-full border-2 border-gray-500 ${
+                  button === "repayment" ? "bg-blue-500" : "bg-transparent"
+                }`}
+              ></div>
+            </button>
+            <button type="button" onClick={() => setButton("interest")}>
+              Interest Only
+              <div
+                className={`flex h-5 w-5 cursor-pointer items-center justify-center rounded-full border-2 border-gray-500 ${
+                  button === "interest" ? "bg-blue-500" : "bg-transparent"
+                }`}
+              ></div>
+            </button>
+            {error && <span>{error}</span>}
           </label>
           <button type="submit">
             <img src={icon} />
